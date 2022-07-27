d261f6177d850791059ff8418e9988b3132b92d6
zao111222333 <zaozao12138@163.com>
2022-07-28 03:18:13 +0800

    update env.js

--- env.js
+++ env.js
@@ -1,12 +1,10 @@
 'use strict';
 const { exec } = require("child_process");
 var command = require('./lib/command');
+var config = require('./lib/config');
 
-const socketDir = '/run';
-const bootConnectUser = false;
-
-exec(command.setConnect('root',socketDir),{shell: "/bin/bash"});
-exec(command.setConnect('admin',socketDir),{shell: "/bin/bash"});
+exec(command.setConnect('root',config.socketDir),{shell: "/bin/bash"});
+exec(command.setConnect('admin',config.socketDir),{shell: "/bin/bash"});
 exec(command.createUser('admin','123456',true),{shell: "/bin/bash"});
 exec(command.createUser('test','123456',false),{shell: "/bin/bash"});
 exec("sleep 5 && cat /root/error.txt && cat /root/output.txt",{shell: "/bin/bash"},(error, stdout, stderr) => {
